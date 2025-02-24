01b0aefd714861cbaa21edb404b692f313f70ce3
Joe-Wehbe <joewehbe@yahoo.com>
2025-02-24 17:38:19 +0200

    Worked on the total component

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
 import { SideMenuComponent } from "../side-menu/side-menu.component";
+import { TotalComponent } from "../total/total.component";
 
 @Component({
   selector: 'app-main',
-  imports: [HeaderComponent, SideMenuComponent],
+  imports: [HeaderComponent, SideMenuComponent, TotalComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
