a46cf338ab663e772e51208973d83bb985360a80
Joe-Wehbe <joewehbe@yahoo.com>
2025-03-14 12:07:51 +0200

    Improved responsiveness

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./components/header/header.component";
-import { SideMenuComponent } from "./components/side-menu/side-menu.component";
 import { MainComponent } from './components/main/main.component';
 
 @Component({
   standalone: true,
   selector: 'app-root',
-  imports: [RouterOutlet, MainComponent],
+  imports: [MainComponent], // add RouterOutlet
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
