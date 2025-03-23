a84feafc97533a3cc313e8ed90a8069431edd1e8
Joe-Wehbe <joewehbe@yahoo.com>
2025-03-23 12:11:44 +0200

    Improved the design

--- src/app/components/dashboard/accounts/accounts.component.ts
+++ src/app/components/dashboard/accounts/accounts.component.ts
@@ -2,16 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { AccountsService } from '../../../services/accounts.service';
 import { Account } from '../../../models/account.model';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-accounts',
-  imports: [RouterLink],
+  imports: [RouterLink, CurrencyPipe],
   templateUrl: './accounts.component.html',
   styleUrl: './accounts.component.css'
 })
-export class AccountsComponent implements OnInit{
+export class AccountsComponent implements OnInit {
   isLoading = true;
   accounts: Account[] = [];
+  selectedId: number | null = null;
+  allSelected = true;
 
   constructor(private accountsService: AccountsService) {}
 
@@ -20,6 +23,21 @@ export class AccountsComponent implements OnInit{
       this.isLoading = false;
     }, 500);
 
-    this.accounts = this.accountsService.getAccounts(); 
+    this.accounts = this.accountsService.getAccounts();
   }
+
+  selectAccount(id: number): void {
+    this.selectedId = id;
+    this.allSelected = false
+  }
+
+  selectAll() {
+    this.allSelected = !this.allSelected;
+    if (!this.allSelected && this.accounts.length > 0) {
+      this.selectedId = this.accounts[0].id;
+    } else {
+      this.selectedId = null;
+    }
+  }
+  
 }
