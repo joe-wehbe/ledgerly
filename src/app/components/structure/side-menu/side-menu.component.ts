0320b7608fc3312245188008e938e4f468703947
Joe-Wehbe <joewehbe@yahoo.com>
2025-04-14 16:45:14 +0300

    Designed the export feature

--- src/app/components/structure/side-menu/side-menu.component.ts
+++ src/app/components/structure/side-menu/side-menu.component.ts
@@ -1,15 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { MenuToggleService } from '../../../services/utility/menu-toggle.service';
+import { AccountsService } from '../../../services/accounts.service';
+import { Account } from '../../../models/account.model';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-side-menu',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [FormsModule, RouterLink, RouterLinkActive],
   templateUrl: './side-menu.component.html',
   styleUrl: './side-menu.component.css'
 })
-export class SideMenuComponent {
-  constructor(private menuToggleService: MenuToggleService) {}
+export class SideMenuComponent implements OnInit {
+  accounts: Account[] = []
+  selectedAccountId: number | null = 0;
+
+  constructor(private menuToggleService: MenuToggleService, private accountsService: AccountsService) {}
+
+  ngOnInit() {
+    this.accounts = this.accountsService.getAccounts()
+  }
 
   toggleSideMenu() {
     if (window.innerWidth < 1200) {
