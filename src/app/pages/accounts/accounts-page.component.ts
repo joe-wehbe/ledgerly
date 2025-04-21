8513a3bd88bc625ed3ec751d6cd5273343f0f586
Joe-Wehbe <joewehbe@yahoo.com>
2025-04-21 17:50:53 +0300

    Worked on the accounts page

--- src/app/pages/accounts/accounts-page.component.ts
+++ src/app/pages/accounts/accounts-page.component.ts
@@ -1,11 +1,83 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { CurrencyPipe, DatePipe } from '@angular/common';
+import { Account } from '../../models/account.model';
+import { AccountsService } from '../../services/accounts.service';
+import { FormsModule } from '@angular/forms';
+import { SnackbarService } from '../../services/utility/snackbar.service';
 
 @Component({
   selector: 'app-accounts-page',
-  imports: [],
+  imports: [FormsModule, DatePipe, CurrencyPipe],
   templateUrl: './accounts-page.component.html',
   styleUrl: './accounts-page.component.css'
 })
 export class AccountsPageComponent {
+  isLoading = true;
+  accounts: Account[] = []
+  newest = true;
+  searchQuery = '';
+  isModalOpen = false;
+  accountName: string = '';
+  initialBalance: number = 0;
 
+  constructor(private accountsService: AccountsService, private snackBarService: SnackbarService) {}
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 500);
+
+    this.accounts = this.accountsService.getAccounts();
+  }
+
+  filteredAccounts() {
+    const query = this.searchQuery.toLowerCase().trim();
+    return this.accounts
+      .filter(account => account.name.toLowerCase().includes(query))
+      .sort((a, b) => {
+        const dateA = a.createdAt ? new Date(a.createdAt).getTime() : 0;
+        const dateB = b.createdAt ? new Date(b.createdAt).getTime() : 0;
+        return this.newest ? dateB - dateA : dateA - dateB;
+      });
+  }
+  
+  cancelQuery() {
+    this.searchQuery = '';
+  }
+
+  sortByDate() {
+    this.newest = !this.newest;
+  }
+
+  openModal() {
+    this.isModalOpen = true;
+  }
+
+  closeModal() {
+    this.isModalOpen = false; 
+  }
+
+  onSubmit(form: any) {
+    if (form.valid) {
+      this.accountName = this.accountName.split(' ').filter(word => word.trim() !== '')
+        .map(word => word[0].toUpperCase() + word.slice(1).toLowerCase()).join(' ');
+  
+      if (this.accountName != '') {
+        for (let i = 0; i < this.accounts.length; i++) {
+          if (this.accounts[i].name === this.accountName) {
+            this.snackBarService.warning("An account with this name already exists");
+            return;
+          }
+        }
+  
+        this.accountsService.addAccount(this.accountName, this.initialBalance);
+        this.accounts = this.accountsService.getAccounts();
+        this.snackBarService.success("Account added");
+        form.reset();
+        this.closeModal();
+      }
+    } else {
+      this.snackBarService.warning("Invalid input");
+    }
+  }
 }
